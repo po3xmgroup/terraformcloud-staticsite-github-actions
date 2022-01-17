774c923b9ff0f6bdf4687da8e7b16ee7e9767262
po3xmgroup <pablo.ojeda@3xmgroup.com>
2022-01-16 22:21:29 -0300

    add branch dev

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "aws_codepipeline" {
+  source = "./modules/aws_codepipeline"
+ 
+
+}
+
+module "s3_module" {
+  source = "./modules/aws_s3"
+  s3_bucket_name = "s3-static-website-training"
+}
---
+++ modules/aws_s3/aws_s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = var.s3_bucket_name
+  acl    = "public-read"
+  policy = templatefile("./templates/policy_s3.json", { aws_s3_bucket = "s3-static-website-training" })
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+
+  }
+  tags = {
+    project   = "training"
+    createdBy = "pabloOjeda"
+  }
+}
